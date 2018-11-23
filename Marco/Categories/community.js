7303056f357f9eade28046fe9cbb3d767c12e1ae
Marco <mcg.tampieri@gmail.com>
2018-11-23 11:54:31 +0100

    push for jeremy

--- Marco/Categories/community.js
+++ Marco/Categories/community.js
@@ -1,7 +1,7 @@
 let myVar = document.querySelector(".dropbtn")
 let othervar = document.querySelector("#myDropdown")
 
-const myFunc = () => {
+const dropMenu = () => {
     if (othervar.style.display == "block"){
         othervar.style.display = "none"
     } else {
@@ -10,4 +10,18 @@ const myFunc = () => {
     }
 }
 
-myVar.addEventListener("click", myFunc);
+myVar.addEventListener("click", dropMenu);
+
+
+let search = document.querySelector(".loupe")
+let inputs = document.querySelector("#searchDrop")
+
+const dropSearch = () => {
+    if (inputs.style.display == "block"){
+        inputs.style.display = "none"
+    } else {
+        inputs.style.display = "block"
+    }
+}
+
+search.addEventListener("click", dropSearch);
