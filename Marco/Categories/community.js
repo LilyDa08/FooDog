8a62b339b1c16e01ef31c9a7a26f70f4b41eee8a
Marco <mcg.tampieri@gmail.com>
2018-11-28 10:06:50 +0100

    removed drop-down searchbar, added modal window for search

--- Marco/Categories/community.js
+++ Marco/Categories/community.js
@@ -14,18 +14,39 @@ myVar.addEventListener("click", dropMenu);
 
 
 let search = document.querySelector(".loupe")
-let inputs = document.querySelector("#searchDrop")
 
-const dropSearch = () => {
-    if (inputs.style.display == "block") {
-        inputs.style.display = "none"
-    } else {
-        inputs.style.display = "block"
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+search.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
 }
 
-search.addEventListener("click", dropSearch);
 
+
+
+
+
+
+// AJAX REQUESTS BELOW XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 
 let xhr = new XMLHttpRequest();
